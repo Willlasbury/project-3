6ccde631e3ca411f2a6b954067d5d80f99a3ebf0
Willlasbury <asburyw229@gmail.com>
2023-05-30 10:22:51 -0700

    Updates

---
+++ precache-manifest.d1cc15c6e6f09fd98e4bfd9909ff272f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "037f36f121f25ff73108",
+    "url": "/Willlasbury/project-3-front/static/css/main.0b2a6441.chunk.css"
+  },
+  {
+    "revision": "037f36f121f25ff73108",
+    "url": "/Willlasbury/project-3-front/static/js/main.037f36f1.chunk.js"
+  },
+  {
+    "revision": "d4293f1070b9d8d68333",
+    "url": "/Willlasbury/project-3-front/static/js/1.d4293f10.chunk.js"
+  },
+  {
+    "revision": "eeef3196f8df98f415ce",
+    "url": "/Willlasbury/project-3-front/static/js/runtime~main.eeef3196.js"
+  },
+  {
+    "revision": "cb8ba42b47afa705e6a817ac44dd09b6",
+    "url": "/Willlasbury/project-3-front/index.html"
+  }
+];
