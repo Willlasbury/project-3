d91886eca64a0ee6cbf079b8364e3be9119c2e04
Willlasbury <asburyw229@gmail.com>
2023-06-05 20:06:53 -0700

    removed preset test values from signup page:

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -6,12 +6,11 @@ import usersAPI from "../../utils/API/users";
 import "../../index.css";
 
 export default function Signup(props) {
-  const navigate = useNavigate()
-  const [name, setName] = useState("wiFDSAll");
-  const [email, setEmail] = useState("will@wiFDSAll.will");
-  const [password, setPassword] = useState("password");
-  const [confirmPassword, setConfirmPassword] = useState("password");
-
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -36,14 +35,13 @@ export default function Signup(props) {
         setName("");
         setPassword("");
         setConfirmPassword("");
-        navigate('/')
+        navigate("/");
       }
 
       setEmail("");
       setName("");
       setPassword("");
       setConfirmPassword("");
-  
     }
   };
 
@@ -53,46 +51,83 @@ export default function Signup(props) {
     <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Default input</label>
     <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
 </div> */}
-<div class="mb-3">
-    <label for="Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-    <input type="text" id="default-input" class="px-3 border-4 border-gray-700 rounded-lg shadow-lg bg-grey-300 "
-    placeholder="Name"
-    value={name}
-    onChange={(event) => setName(event.target.value)}
-    />
-</div>
-<div class="mb-3">
-    <label for="Email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-    <input type="text" id="default-input" class="px-3 border-4 border-gray-700 rounded-lg shadow-lg bg-grey-300 "
-    placeholder="email"
-    value={email}
-    onChange={(event) => setEmail(event.target.value)}
-    />
-</div>
-<div class="mb-3">
-    <label for="Password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-    <input type="text" id="default-input" class="px-3 border-4 border-gray-700 rounded-lg shadow-lg bg-grey-300 "
-    placeholder="Password"
-    value={password}
-    onChange={(event) => setPassword(event.target.value)}
-    />
-</div>
-<div class="mb-3">
-    <label for="Password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-    <input type="text" id="default-input" class="px-3 border-4 border-gray-700 rounded-lg shadow-lg bg-grey-300 "
-    placeholder="Confirm Password"
-    value={confirmPassword}
-    onChange={(event) => setConfirmPassword(event.target.value)}
-    />
-</div>
-      
-      <button onClick={handleSubmit} className="px-3 border-4 border-blue-950 rounded-lg shadow-lg bg-stone-300 hover:font-bold hover:bg-orange-200 text-xl font-medium">Signup</button>
-      
+      <div class="mb-3">
+        <label
+          for="Name"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Name
+        </label>
+        <input
+          type="text"
+          id="default-input"
+          class="px-3 border-4 border-gray-700 rounded-lg shadow-lg bg-grey-300 "
+          placeholder="Name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+      <div class="mb-3">
+        <label
+          for="Email"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Email
+        </label>
+        <input
+          type="text"
+          id="default-input"
+          class="px-3 border-4 border-gray-700 rounded-lg shadow-lg bg-grey-300 "
+          placeholder="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+      <div class="mb-3">
+        <label
+          for="Password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Password
+        </label>
+        <input
+          type="text"
+          id="default-input"
+          class="px-3 border-4 border-gray-700 rounded-lg shadow-lg bg-grey-300 "
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <div class="mb-3">
+        <label
+          for="Password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Password
+        </label>
+        <input
+          type="text"
+          id="default-input"
+          class="px-3 border-4 border-gray-700 rounded-lg shadow-lg bg-grey-300 "
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+        />
+      </div>
+
+      <button
+        onClick={handleSubmit}
+        className="px-3 border-4 border-blue-950 rounded-lg shadow-lg bg-stone-300 hover:font-bold hover:bg-orange-200 text-xl font-medium"
+      >
+        Signup
+      </button>
     </form>
   );
 }
 
-{/* <input
+{
+  /* <input
         name="name"
         placeholder="Name"
         value={name}
@@ -117,4 +152,5 @@ export default function Signup(props) {
         placeholder="Confirm Password"
         value={confirmPassword}
         onChange={(event) => setConfirmPassword(event.target.value)}
-      /> */}
+      /> */
+}
