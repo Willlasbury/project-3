2aba59005d3c6dacdb8ce2ddd5fa69da4aa08f4a
Michael Campbell <michaelcampbell@Michaels-MBP.domain>
2023-06-06 16:12:57 -0700

    working user verification for page navigation

--- src/pages/FreeItem/index.js
+++ src/pages/FreeItem/index.js
@@ -1,9 +1,18 @@
 import React from "react";
-// import { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./style.css";
 
-export default function FreeItem () {
+export default function FreeItem ({token}) {
+    const navigate = useNavigate()
+  
+    useEffect(()=>{
+      if (!token){
+        navigate('/login')
+      }
+  
+    },[])
     return (
         <div className="flex flex-col items-center mt-20">
         <h1 className="nav-item px-3 border-4 border-stone-950 rounded-lg shadow-lg bg-amber-100 hover:font-bold hover:bg-amber-500 hover:text-stone-900 text-xl font-medium">Free Items</h1>
