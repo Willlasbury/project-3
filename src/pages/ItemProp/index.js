78faf5123e11474caddb485c9fcba0bf828e03ad
Alex Horning <68720206+alexandertbh@users.noreply.github.com>
2023-06-06 14:02:01 -0700

    set up your page

--- src/pages/ItemProp/index.js
+++ src/pages/ItemProp/index.js
@@ -9,18 +9,33 @@ export default function Items({ id, picture, title, category, condition }) {
   console.log("title", title);
   const itemId = id;
   console.log("id:", id);
+  const responsive = {
+    0: { items: 1 },
+    568: { items: 4 },
+    1024: { items: 6 },
+  };
   return (
     <div className="flex flex-col items-center m-3 px-3 border-4 border-stone-950 rounded-lg shadow-lg bg-amber-100 text-xl font-medium">
-    
-      <AliceCarousel className="justify-center">
+      <AliceCarousel
+        mouseTracking
+        infinite
+        responsive={responsive}
+        controlsStrategy={"default"}
+        autoPlayStrategy="all"
+        autoPlayInterval={1000}
+        autoHeight
+      >
         {picture.map((photo, index) => (
-          <img key={index} src={photo.url} className="sliderimg" />
+          <img key={index} src={photo.url} className="sliderimg " />
         ))}
       </AliceCarousel>
       <h3 className="item-title text-black">{title}</h3>
       <p className="item-category text-black"> Category: {category}</p>
       <p className="text-black">Condition: {condition}</p>
-      <Link to={`/item/${id}`} className="item-link px-3 m-2 border-4 border-stone-950 rounded-lg shadow-lg bg-amber-100 hover:font-bold hover:bg-cyan-500 text-xl font-medium">
+      <Link
+        to={`/item/${id}`}
+        className="item-link px-3 m-2 border-4 border-stone-950 rounded-lg shadow-lg bg-amber-100 hover:font-bold hover:bg-cyan-500 text-xl font-medium"
+      >
         View Item
       </Link>
     </div>
--- src/pages/Items/index.js
+++ src/pages/Items/index.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect, useLocation } from "react";
 import itemsAPI from "../../utils/API/items";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
+import "./style.css";
 
 export default function Item() {
   // console.log("id:", id);
   const [items, setItems] = useState({
     Photos: [{}],
   });
+
+  const style = {
+    alignItems: "center",
+  };
 
   // console.log(
   //   "window.location.pathname:",
@@ -33,17 +38,27 @@ export default function Item() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center mt-20">
+    <div className="flex flex-col mt-20">
       <form className="px-3 m-2 border-4 border-stone-950 rounded-lg shadow-lg bg-amber-100 hover:font-bold text-xl font-medium">
-      <h1>Individual Item</h1>
-      <AliceCarousel>
-        {items.Photos.map((photo, index) => (
-          <img key={index} src={photo.url} className="sliderimg" />
-        ))}
-      </AliceCarousel>
-      <h2>Title:{items.title}</h2>
-      <h2>Condition:{items.condition}</h2>
-      <h2>minimum_trade:{items.minimum_trade}</h2>
+        <h1>Individual Item</h1>
+        <AliceCarousel
+          mouseTracking
+          infinite
+          controlsStrategy={"default"}
+          autoPlayStrategy="all"
+          autoPlayInterval={1000}
+          disableDotsControls
+          disableButtonsControls
+          keyboardNavigation
+        >
+          {items.Photos.map((photo, index) => (
+            <img key={index} src={photo.url} className="sliderimg" />
+          ))}
+        </AliceCarousel>
+        <h2>Title:{items.title}</h2>
+        <h2>Condition:{items.condition}</h2>
+        <h2>Minimum Trade Value:{items.minimum_trade}</h2>
+        <h2>Description:{items.description}</h2>
       </form>
     </div>
   );
