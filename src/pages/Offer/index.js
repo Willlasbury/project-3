e60b57299528d8b0d894584f3b1b24407e3bdf2b
Willlasbury <asburyw229@gmail.com>
2023-06-08 13:34:57 -0700

    resolved merge conflicts

--- src/pages/Offer/index.js
+++ src/pages/Offer/index.js
@@ -13,17 +13,6 @@ export default function Offer({ token, socket, userId }) {
     if (!token) {
       navigate("/login");
     }
-<<<<<<< HEAD
-  },[])
-    return (
-    <div className="flex flex-col items-center mt-5">
-    <h1 className="px-3 border-4 border-stone-950 rounded-lg shadow-lg bg-amber-100 hover:font-bold hover:bg-amber-500 hover:text-stone-900 text-xl font-medium"> Click on the Item that you would like to make an offer on</h1>
-    <Link to={`/items`} className="item-link text-black">
-      </Link>
-    </div>
-    );
-}
-=======
   }, []);
 
   let imageArr = [];
@@ -207,4 +196,3 @@ export default function Offer({ token, socket, userId }) {
     </>
   );
 }
->>>>>>> dev
