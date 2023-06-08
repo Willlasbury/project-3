e8be518645c4ba8047f341efdae520065204fac1
Alex Horning <68720206+alexandertbh@users.noreply.github.com>
2023-06-08 14:17:24 -0700

    added where filter on browse

--- src/pages/Browse/index.js
+++ src/pages/Browse/index.js
@@ -4,17 +4,18 @@ import itemsAPI from "../../utils/API/items";
 import "../../index.css";
 import { useNavigate } from "react-router-dom";
 
-export default function Browse({token}) {
+export default function Browse({ token, userId }) {
   const [items, setItems] = useState([]);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   // Fetch items data and update the items state
+  console.log("userId:", userId);
   useEffect(() => {
-    if (!token){
-      navigate('/login')
+    if (!token) {
+      navigate("/login");
     }
     const fetchItems = async () => {
       try {
-        const fetchedItems = await itemsAPI.getItemsBrowse();
+        const fetchedItems = await itemsAPI.getItemsBrowse(userId);
         setItems(fetchedItems);
       } catch (error) {
         console.log("Error fetching items:", error);
@@ -35,7 +36,7 @@ export default function Browse({token}) {
             key={item.id}
             className="card mx-auto my-4 p-4 max-w-sm bg-amber-100 rounded-lg shadow-lg text-center"
           >
-            <Item 
+            <Item
               id={item.id}
               picture={item.Photos}
               title={item.title}
